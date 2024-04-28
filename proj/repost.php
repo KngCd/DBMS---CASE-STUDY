7d7378a318df0acdbe521914968a9f283fd5c108
kil_zldyck <148508745+KngCd@users.noreply.github.com>
2024-04-28 18:16:03 +0800

    Add files via upload

---
+++ proj/repost.php
@@ -0,0 +1,19 @@
+<?php
+$classcode = $_POST['classcode'];
+$act_id = $_POST['act_id'];
+$topic = $_POST['topic'];
+$description = $_POST['description'];
+$points = $_POST['points'];
+$due_date = $_POST['due_date'];
+$time = $_POST['time'];
+$filetype = $_POST['filetype'];
+
+echo $classcode;
+echo $act_id;
+echo $topic;
+echo $description;
+echo $points;
+echo $due_date;
+echo $time;
+echo $filetype;
+?>
